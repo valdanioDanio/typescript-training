016dd838f435b8ca85cfd72b8fdd1c98cf9d8299
valdanioDanio <38763412+valdanioDanio@users.noreply.github.com>
2023-07-28 15:17:16 +0000

    promises js

--- functions.js
+++ functions.js
@@ -1,4 +1,25 @@
-function simplify(nome, isactive) {
-    return nome;
-}
-console.log(simplify("Valdanio", false));
+function f(g,h){
+
+    if( 10 > 10){
+      g("O nosso P")
+    }else{
+      h("O vosso Q")
+    }
+  
+  }
+  
+  function executarSeRejeitado(valor){
+    console.log("Rejeitada "+valor);
+  }
+  
+  function executarSeAciete(valor){
+    console.log("Aceite "+valor);
+  }
+  
+  const minhaPromise = new Promise(f);
+
+  minhaPromise.then(valor =>{
+    console.log("Resolveu "+valor);
+  }).catch(valor=>{
+    console.log("Não resolveu "+valor);
+  })
--- functions.ts
+++ functions.ts
@@ -2,4 +2,13 @@ function simplify(nome: string, isactive: boolean) : string {
   return nome;
 }
 
-console.log(simplify("Valdanio Alberto", false));
+//console.log(simplify("Valdanio Alberto", false));
+
+async function test(): Promise<any>  {
+  let data: any = await "default string";
+  console.log("The value of data is " + data);
+}
+
+console.log("Before function execution");
+test();
+console.log("After function execution");
